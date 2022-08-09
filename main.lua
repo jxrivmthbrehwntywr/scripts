f9a266b92f5416ad748af62c5a3fc7a533411bb1
jxrivmthbrehwntywr <108086564+jxrivmthbrehwntywr@users.noreply.github.com>
2022-08-09 06:04:09 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,12 @@
+-- Thunder Z HUB 
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ThunderZ-05/HUB/main/Script"))()
+
+-- Project Meow 
+
+loadstring(game:HttpGet"https://rawscripts.net/raw/Project-Meow_421")()
+--Key https://discord.gg/JyREQDDMdf
+
+
+
+
